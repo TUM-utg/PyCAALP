b05f2ed93be7e4e516fc4be43cc97d90b24bc8f6
Marios Demetriades <marios.demetriades@tum.de>
2026-05-04 08:22:20 +0200

    Assembly 2: edge reduction experiment n=5

--- experiments/edge_reduction/sample_tests/assembly_2_n_p_3/config.py
+++ experiments/edge_reduction/sample_tests/assembly_2_n_p_3/config.py
@@ -2,7 +2,7 @@
 
 config_data = {
     "assembly_fname": "data/assembly_2/assembly_2_parts.json",
-    "dfm_fname": "",  # "" if no DFM
+    "dfm_fname": "data/assembly_2/assembly_2_dfm.json",  # "" if no DFM
     "res_fname": "experiments/edge_reduction/sample_tests/assembly_2_n_p_3/assembly_2.pkl",
     # Defaults to 0.5 i.e., equal contribution of engineering constraints and time balancing
     "w_balanced": 0.8,
@@ -10,7 +10,7 @@ config_data = {
     "w_edge_reduction": [0, 10, 70],
     "num_phases": 3,
     "num_rep_runs": 5,
-    "relative_gap": 0.05,  # MIP solver relative gap [0-1]
+    "relative_gap": 0.00,  # MIP solver relative gap [0-1]
     "plots": [
         "objective_value",
         "total_time",
