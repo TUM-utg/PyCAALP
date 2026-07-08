e57b9d7d37f1c3ddaa247b18c0ff57b7a7df3fef
Marios Demetriades <marios.demetriades@tum.de>
2026-07-08 12:22:02 +0200

    Removed uneccessary experiments for WGP

--- experiments/strategy_comparison/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=$PYTHONPATH:.
-
-echo "Starting Experiment: strategy_comparison ..."
-python -m experiments.strategy_comparison.test_strategy_comparison
-
-echo "Experiment complete."
